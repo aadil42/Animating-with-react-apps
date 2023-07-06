64b488a97f8e240429db5b88451e1963d7cf6e8b
aadil42 <adilkelawala@gmail.com>
2023-07-06 10:50:52 +0530

    adding animation in model

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from "react-transition-group/Transition";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -8,7 +8,8 @@ import List from "./components/List/List";
 class App extends Component {
   
   state = {
-    isModleOpen: false
+    isModleOpen: false,
+    isTransition: true
   }
 
   closeModel = () => {
@@ -19,12 +20,41 @@ class App extends Component {
     this.setState({isModleOpen:  true});
   }
 
+  toggleTransition = () => {
+    this.setState({isTransition: !this.state.isTransition});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        {this.state.isModleOpen && <Modal isOpen={this.state.isModleOpen} closed={this.closeModel} />}
+        <Modal isOpen={this.state.isModleOpen} closed={this.closeModel} />
         {this.state.isModleOpen && <Backdrop isOpen={this.state.isModleOpen} /> }
+        {/* <button onClick={this.toggleTransition} className="Button">Transition</button> */}
+        {/* <Transition 
+
+        in={this.state.isTransition} 
+        timeout={300}
+        mountOnEnter
+        unmountOnExit
+        >
+          {(state) => { // state will be either exiting, exited, entering and entered.
+            return (
+              <div
+              style={{
+                width: 100,
+                height: 100,
+                backgroundColor: 'red',
+                transition: 'opaticy 1s ease-out',
+                margin: 'auto',
+                opacity: state === 'exiting' ? 0 : 1
+              }}
+              >
+                {state}
+              </div>
+            );
+          }}
+        </Transition> */}
         <button onClick={this.openModel} className="Button">Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,20 +1,34 @@
 import React from 'react';
-
+import Transition from "react-transition-group/Transition";
 import './Modal.css';
 
-const modal = (props) =>  {
-
-      const cssClasses = [
-        "Modal",
-        props.isOpen ? "ModalOpen" : "ModalClosed"
-      ];
-
+const animationTimeOut = {
+    enter: 400,
+    exit: 1000
+}
+const Modal = (props) =>  {
       return (
-        <div className={cssClasses.join(' ')}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>Dismiss</button>
-        </div>
+        <Transition
+        in={props.isOpen}
+        timeout={animationTimeOut}
+        mountOnEnter
+        unmountOnExit
+        >
+            {(state) => {
+                const cssClasses = [
+                "Modal",
+                state === 'entering' ? 'ModalOpen' 
+                : state === 'exiting' ? 'ModalClosed' : null
+                ];
+                return(
+                <div className={cssClasses.join(' ')}>
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+                </div>
+                );
+        }}
+        </Transition>
       );
 }
 
-export default modal;
+export default Modal;
